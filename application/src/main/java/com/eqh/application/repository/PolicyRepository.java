c06923653f1707cd19f2f02040c411664bedf790
rthota7 <rthota7@dxc.com>
2024-08-12 20:07:57 +0530

    removed terminated and home office cancellation data

--- application/src/main/java/com/eqh/application/repository/PolicyRepository.java
+++ application/src/main/java/com/eqh/application/repository/PolicyRepository.java
@@ -7,7 +7,15 @@ import org.springframework.data.repository.query.Param;
 
 public interface PolicyRepository extends JpaRepository<Policy, Long> {
 
-    @Query(value="SELECT product_code FROM public.\"POLICY\" WHERE pol_number = :polNumber AND policy_status != 'R'", nativeQuery = true)
+    /*
+    *
+    * policy status - R reprsents soft deleted.
+    * policy status - 14 is terminated.
+    * policy status - 13 is Home Office Cancellation.
+    *
+    * */
+    @Query(value="SELECT product_code FROM public.\"POLICY\" WHERE pol_number = :polNumber AND policy_status NOT IN ('R', '14', '13')", nativeQuery = true)
     String findProductCodeByPolicyNumber(@Param("polNumber") String polNumber);
+
 }
 
